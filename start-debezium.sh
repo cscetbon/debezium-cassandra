d6a2b9efe974c199641f2c642cdec8eec63facf4
Cyril Scetbon <cscetbon@gmail.com>
2020-05-07 23:53:15 -0400

    Upgrade to 1.2.0.Beta1

--- start-debezium.sh
+++ start-debezium.sh
@@ -1,1 +1,1 @@
-java -jar debezium-connector-cassandra-1.1.0.Final-jar-with-dependencies.jar /etc/debezium/cdc.properties > debezium.stdout.log 2> debezium.stderr.log
+java -jar debezium-connector-cassandra-jar-with-dependencies.jar /etc/debezium/cdc.properties > debezium.stdout.log 2> debezium.stderr.log
